cd969ae5adaa11ae36391b7449cfc8ca64eaedc3
Elliot Morgan <elliotmrgn@gmail.com>
2020-07-17 20:36:35 -0400

    api calls working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React, { useState, useEffect } from "react";
-import logo from './logo.svg';
-import VideoPlayer from './components/videoPlayer'
-import './App.css';
-import API from './utils/API'
+import logo from "./logo.svg";
+import VideoPlayer from "./components/videoPlayer";
+import "./App.css";
+import API from "./utils/API";
 
 function App() {
-  
-  useEffect(()=>{
-    API.bpmResults().then(res=>{
-      console.log(res.data.tempo[0])
-      console.log(res.data.tempo[1])
-    })
-  },[])
+  let sourceID;
+
+  useEffect(() => {
+    API.bpmResults()
+      .then((res) => {
+        console.log("SONG 1!!", res.data.tempo[0]);
+        console.log("SONG 2!!", res.data.tempo[1]);
+      })
+      .then(() => {
+        API.musicVideoSearch().then((res) => {
+          console.log("MUSIC VIDEO RESULTS!!", res.data.results[0]);
+          API.musicVideoSource(res.data.results[0].id)
+            .then((res) => {
+              console.log(
+                "**MUSIC VIDEO SOURCE",
+                res.data.sources[0].source_data
+              );
+              sourceID = res.data.sources[0].source_data;
+            })
+        });
+      });
+  }, []);
 
   return (
     <div className="App">
--- src/utils/API.js
+++ src/utils/API.js
@@ -6,11 +6,20 @@ export default {
       "https://api.getsongbpm.com/tempo/?api_key=a2ac4e9276908567b494bc67129c5ccd&bpm=135"
     );
   },
-  musicVideo: function () {
-    return axios.get("http://imvdb.com/api/v1/search/videos", {
-      headers: {
-        'IMVDB-APP-KEY':  'gKLRKM6um3XrLKXjtOhOeVBpQRf4cCNYcMaP4hBB' 
+  musicVideoSearch: function () {
+    return axios({method:'GET', url:"https://imvdb.com/api/v1/search/videos?q=Abba+Mamma+Mia", headers: {
+        "IMVDB-APP-KEY": "gKLRKM6um3XrLKXjtOhOeVBpQRf4cCNYcMaP4hBB",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Origin": "http://localhost:3000",
+        "Access-Control-Allow-Methods":
+          "GET, POST, PATCH, DELETE, PUT, OPTIONS",
+        "Access-Control-Allow-Headers":
+          "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
       },
     });
   },
+  musicVideoSource: function(id){
+    return axios.get(`https://imvdb.com/api/v1/video/${id}?include=sources`, {headers:{"IMVDB-APP-KEY": "gKLRKM6um3XrLKXjtOhOeVBpQRf4cCNYcMaP4hBB"}})
+  }
 };
