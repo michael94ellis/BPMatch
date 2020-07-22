7ee4b361316c9aa451ddac95c44ad19538992ed9
Elliot Morgan <elliotmrgn@gmail.com>
2020-07-22 00:28:03 -0400

    random match

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import UserInput from "./components/UserInput";
 import VideoPlayer from "./components/videoPlayer";
 import "./App.css";
@@ -6,8 +6,10 @@ import API from "./utils/API";
 
 const App = () => {
   const [audioID, setAudioID] = useState();
-  const [search, setSearch] = useState();
+  const [search, setSearch] = useState("");
   const [videoID, setVideoID] = useState();
+  const audioRef = useRef();
+  const videoRef = useRef();
 
   const handleChange = (event) => {
     setSearch(event.target.value);
@@ -15,44 +17,56 @@ const App = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    API.bpmLookup(search)
-      .then((res) => {
-        console.log("SONG 1!!", res.data.search[0].title);
-        console.log("SONG 1!!", res.data.search[0].artist.name);
-        
-        API.musicVideoSearch(res.data.search[0].artist.name, res.data.search[0].title).then((res) => {
-          console.log("MUSIC VIDEO RESULTS!!", res);
-          API.musicVideoSource(res.data.results[0].id).then((res) => {
-            console.log(
-              "**MUSIC VIDEO SOURCE",
-              res.data.sources[0].source_data
-            );
-            setAudioID(res.data.sources[0].source_data);
-          });
-        });
+    API.bpmLookup(search).then((res) => {
+      console.log("SONG 1 title!!:  ", res.data.search[0].title);
+      console.log("SONG 1 artist!!: ", res.data.search[0].artist.name);
 
-        API.bpmResults(res.data.search[0].id).then((res) => {
-          API.bpmMatch(res.data.song.tempo).then((res) => {
-            console.log("~~~~~~~~~~~~~~", res.data.tempo[0].song_title);
-            console.log("***********", res.data.tempo[0].artist.name);
-            
-            API.musicVideoSearch(res.data.tempo[0].artist.name, res.data.tempo[0].song_title).then((res) => {
-              console.log("MUSIC VIDEO RESULTS!!", res);
-              API.musicVideoSource(res.data.results[0].id).then((res) => {
-                console.log(
-                  "**MUSIC VIDEO SOURCE",
-                  res.data.sources[0].source_data
-                );
-                setVideoID(res.data.sources[0].source_data);
-              });
+      API.musicVideoSearch(
+        res.data.search[0].artist.name,
+        res.data.search[0].title
+      ).then((res) => {
+        console.log("MUSIC VIDEO RESULTS!!", res);
+        API.musicVideoSource(res.data.results[0].id).then((res) => {
+          
+          console.log("**MUSIC VIDEO SOURCE", res.data.sources[0].source_data);
+          setAudioID(res.data.sources[0].source_data);
+        });
+      });
+
+      API.bpmResults(res.data.search[0].id).then((res) => {
+        API.bpmMatch(res.data.song.tempo).then((res) => {
+          let i = Math.floor(Math.random()*res.data.tempo.length)
+          console.log("~~~~~~~~~~~~~~", res.data.tempo);
+          console.log("***********", res.data.tempo[i].artist.name);
+
+          API.musicVideoSearch(
+            res.data.tempo[i].artist.name,
+            res.data.tempo[i].song_title
+          ).then((res) => {
+            console.log("MUSIC VIDEO RESULTS!!", res);
+            API.musicVideoSource(res.data.results[0].id).then((res) => {
+              console.log(
+                "**MUSIC VIDEO SOURCE",
+                res.data.sources[0].source_data
+              );
+              setVideoID(res.data.sources[0].source_data);
             });
-
           });
         });
-      })
+      });
+    });
+  };
 
-      
-      
+  const stateChange = (event) => {
+    console.log("State changed", event.target.getPlayerState());
+    console.log(audioRef);
+    // if (event.target.getPlayerState() === 5) {
+    //   ready++;
+    //   console.log("TEST", ready)
+    //   if (ready === 2) {
+    //     event.target.playVideo();
+    //   }
+    // }
   };
 
   return (
@@ -62,9 +76,8 @@ const App = () => {
         handleChange={handleChange}
         results={search}
       />
-      <VideoPlayer id={audioID} />
-      <VideoPlayer id={videoID} />
-      
+      <VideoPlayer id={audioID} stateChange={stateChange} ref={audioRef} />
+      <VideoPlayer id={videoID} stateChange={stateChange} />
     </div>
   );
 };
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,16 +1,35 @@
-import React from 'react';
-import YouTube from 'react-youtube';
+import React from "react";
+import YouTube from "react-youtube";
 
-const VideoPlayer = (props) =>{
-    const opts = {
-        height: '390',
-        width: '640',
-        playerVars: {
-          // https://developers.google.com/youtube/player_parameters
-          autoplay: 1,
-        }
-    }
-    return <YouTube videoId= {props.id}opts={opts}/>
-}
+const VideoPlayer = (props) => {
+  const onReady = (event) => {
+    console.log("READY", event.target);
+  };
+
+  const stateChange = (event)=>{
+    console.log("State changed",props.id, event.target.getPlayerState())
+  } 
+
+  const onError = () => {
+    console.log("ERROR");
+  };
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 0,
+    },
+  };
+  return (
+    <YouTube
+      videoId={props.id}
+      opts={opts}
+      onReady={onReady}
+      onError={onError}
+      onStateChange={props.stateChange}
+    />
+  );
+};
 
 export default VideoPlayer;
